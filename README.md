4bd446f76b233a7c89121f0d3bb12d2431ca6869
Victor Ribeiro <victorqribeiro@gmail.com>
2019-07-03 19:05:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# keypad2text
+
+Transform text to keypad code and vice versa.
+
+![screenshot](screenshot.png)
+
+## About
+
+This is a silly project that I made a while back. I am from an era where you had to text people using the [keypad](https://en.wikipedia.org/wiki/Keypad) of your cell phone. Want to send the letter 's'? You better press the 7 key 5 times.
+
+## How to use
+
+*Text to Keypad*
+Enter text on the input field and click convert to generate keypad code.
+
+*Keypad to Text*
+Enter the keypad code on the input field and click convert to generate text.
---
+++ js/main.js
@@ -0,0 +1,67 @@
+let btn = document.getElementById("btn");
+let cls = document.getElementById("cls");
+
+const matrix = [
+					['1',' ','.',',','!'],
+					['2','a','b','c'],
+          ['3','d','e','f'],
+          ['4','g','h','i'],
+          ['5','j','k','l'],
+          ['6','m','n','o'],
+          ['7','p','q','r','s'],
+          ['8','t','u','v'],
+          ['9','w','x','y','z'],
+          ['0','-','_','@','*','+']
+];
+
+function translate(){
+	txt = document.getElementById("txt");
+  cel = document.getElementById("cel");
+  value = txt.value.normalize('NFD').toLowerCase();
+  if(value){
+  	if(isNaN(value)){
+    	cel.value = textToNum(value);
+    }else{
+    	cel.value = numToText(value);
+    }
+  }else{
+  	btn.value = '???';
+    setTimeout(()=>{ btn.value = "Convert"; },1000);
+  }
+}
+
+function textToNum(txt){
+	let string = "";
+  for(let i = 0; i < txt.length; i++){
+    for(let x = 0; x < matrix.length; x++){
+    	if(matrix[x].indexOf(txt[i]) != -1){
+      	string += x+''+matrix[x].indexOf(txt[i])+'';
+      }
+    }
+  }
+	return string;
+}
+
+function numToText(num){
+	if( num.length % 2)
+  	return "invalid";
+	let string = "";
+	for(let i = 0; i < num.length; i+=2){
+      let i1 = parseInt(num[i]);
+      let i2 = parseInt(num[i+1]);
+      string += matrix[i1][i2];
+  }
+  return string;
+}
+
+btn.addEventListener("click",translate);
+
+cls.addEventListener("click",()=>{
+	txt.value = cel.value = null;
+});
+
+document.body.addEventListener("keypress",e=>{
+	if(e.keyCode == 13){
+  	translate();
+  }
+});
